77c2ced534634973123a01c406acf33319a3cf60
Adept <adeptiumion@gmail.com>
2024-03-01 12:11:23 +0700

    1

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,19 +1,19 @@
 package web.dao;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Repository
 public class CarDaoImpl implements CarDao {
     private List<Car> cars = new ArrayList<>();
 
     {
         cars.add(new Car("Nissan", "Blue", 300));
         cars.add(new Car("Lexus", "Orange", 1100));
-        cars.add(new Car("Mitsubishi", "Black", 900));
         cars.add(new Car("Honda", "Red", 320));
         cars.add(new Car("Toyota", "White", 800));
         cars.add(new Car("Shoha", "Grey", 100));
@@ -21,6 +21,6 @@ public class CarDaoImpl implements CarDao {
 
     @Override
     public List<Car> getCar(int count) {
-        return count >= 5 ? cars.stream().toList() : cars.stream().limit(count).toList();
+        return cars.stream().limit(count).toList();
     }
 }
